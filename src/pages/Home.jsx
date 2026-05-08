adebb29b730a9b4980075c365385b9f90c15515e
kaustav Mani Deka <chwapnamkashyap@gmail.com>
2026-05-09 00:36:10 +0530

    heading

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 const Home = () => {
   return (
     <div className="flex items-center justify-center h-screen bg-gray-100">
-      <h1 className="text-3xl font-bold">Welcome to GitHub API App</h1>
+      <h2 className="text-3xl font-bold">Welcome to GitHub API App</h2>
     </div>
   );
 };
